5b00beb7e199dd22b19b4ef2fbeb775f34cd0703
elyorbekanvarov <e0275king@gmail.com>
2024-05-29 23:36:10 +0500

    6-dars

---
+++ js/index.js
@@ -0,0 +1,77 @@
+// 1 - masala
+
+// for (let i = 1; i <= 50; i++) {
+//   if (i % 2 !== 0) {
+//     console.log(i * i);
+//   }
+// }
+
+// 2 - masala
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log(i * i * i);
+// }
+
+// 3 - masala
+
+// for (let i = 1; i <= 20; i++) {
+//   let factorial = 1;
+//   for (let j = 1; j <= i; j++) {
+//     factorial *= j;
+//   }
+//   console.log(factorial);
+// }
+
+// 4 - masala
+
+// let sum = 0;
+// for (let i = 1; i <= 30; i++) {
+//   sum += i * i;
+// }
+// console.log(sum);
+
+// 5 - masala
+
+// for (let i = 2; i <= 20; i++) {
+//   let isPrime = true;
+//   for (let j = 2; j < i; j++) {
+//     if (i % j === 0) {
+//       isPrime = false;
+//       break;
+//     }
+//   }
+//   if (isPrime) {
+//     console.log(i);
+//   }
+// }
+
+// 6 - masala
+
+// for (let i = 1; i <= 100; i++) {
+//   if (i * 2 < 100) {
+//     console.log(i);
+//   }
+// }
+
+// 7 -masala
+
+// let n = 100;
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 7 == 0 && i%5 !== 0) {
+//         console.log(i);
+//     }
+// }
+
+// 8 - masala
+
+// let a = +prompt("a sonni kiriting;");
+
+// let sum = 0 ;
+
+// for(let i = 1 ; i<=a ; i++){
+// if(a % i == 0 ){
+// console.log(i);
+// sum+=i ;
+// }
+// }
+// console.log(sum);
